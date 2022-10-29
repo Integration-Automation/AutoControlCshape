88755551e38f0c3d46b5f8fa03d619e55ffb3082
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2022-10-29 17:53:08 +0800

    add project and dir
    add project and dir

---
+++ AutoControlCshape/ClientSocket/ClientSocket.cs
@@ -0,0 +1,18 @@
+﻿namespace AutoControlCshape.Socket;
+
+public class ClientSocket
+{
+    private System.Net.Sockets.Socket _socket;
+    private String host;
+    private int port;
+
+    public ClientSocket()
+    {
+        
+    }
+
+    public void CloseClient()
+    {
+        _socket.Close();
+    }
+}
---
+++ AutoControlCshape/DriverManager/DriverManager.cs
@@ -0,0 +1,27 @@
+﻿namespace AutoControlCshape.DriverManager;
+
+public class DriverManager
+{
+
+    private String driverPath;
+    private String host;
+    private int port;
+    
+    public DriverManager(String serverHost, int serverPort, String driverPath)
+    {
+        this.driverPath = driverPath;
+        SetDriver(serverHost, serverPort);
+    }
+
+    private String SetDriver(String serverHost, int serverPort)
+    {
+        if (this.driverPath.Equals(""))
+        {
+            this.driverPath = Directory.GetCurrentDirectory() + "/generate_autocontrol_driver_win64.exe";
+        }
+        host = serverHost;
+        port = serverPort;
+        return driverPath;
+    }
+    
+}
